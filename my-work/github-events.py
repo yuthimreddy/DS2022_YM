5f7780f9d4d5978d5563486777cc875ea3a8490a
yuthimreddy <hva4zb@virginia.edu>
2024-09-22 14:51:32 -0400

    Part 3: Lab3

---
+++ my-work/github-events.py
@@ -0,0 +1,19 @@
+#!/Users/yuthimadireddy/anaconda3/bin/python
+
+import os
+import json
+import requests
+
+GHUSER=os.getenv('GITHUB_USER')
+print(GHUSER)
+
+url = 'https://api.github.com/users/{0}/events'.format(GHUSER)
+print(url)
+
+r = json.loads(requests.get(url).text)
+
+for x in r[:5]:
+  event = x['type'] + ' :: ' + x['repo']['name']
+  print(event)
+
+print(r)
